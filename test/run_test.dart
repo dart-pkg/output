ac860c567b6fcef2a1381e39f15013dbf49c0929
JavaCommons Technologies <javacommons@gmail.com>
2025-04-20 22:26:35 +0900

    Added: echoJson() and dumpJson()

--- test/run_test.dart
+++ test/run_test.dart
@@ -6,8 +6,8 @@ void main() {
     test('run1', () {
       echo([123, null, 'abc']);
       dump([123, null, 'abc']);
-      echoJson([123, null, 'abc']);
-      dumpJson([123, null, 'abc']);
+      echoJson([123, null, 'abc']); // 1.0.2
+      dumpJson([123, null, 'abc']); // 1.0.2
     });
     test('run2', () {
       dump('this is run2');
